9d9910d78f7586e81b25529e127a81e7387a9101
vilena81 <shadyanvilena@gmail.com>
2023-03-24 12:14:34 +0400

    ignore

--- functions/checkAdmin.js
+++ functions/checkAdmin.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 function checkAdmin(req, res){
     const tokenadmin = req.headers.authorization;
     const decoded = jwt.decode(tokenadmin)
-    console.log(decoded)
+
     if (decoded.role === 0){
       return true
     } else {
